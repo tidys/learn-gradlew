05b74d187ee337d38179a755ddb1c823de147c1c
cc <xu_yanfeng@126.com>
2023-03-08 14:23:22 +0800

    111

--- app/src/main/java/learn/gradlew/App.java
+++ app/src/main/java/learn/gradlew/App.java
@@ -5,7 +5,7 @@ package learn.gradlew;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World~!";
     }
 
     public static void main(String[] args) {
